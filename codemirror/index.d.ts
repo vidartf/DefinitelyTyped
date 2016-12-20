fe6de3867d4b9fe286e3d38e6603b2940a31e818
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-12-20 11:21:00 +0100

    Add `curOp`/operation typing

--- codemirror/index.d.ts
+++ codemirror/index.d.ts
@@ -436,6 +436,54 @@ declare namespace CodeMirror {
 
         /** Expose the state object, so that the Editor.state.completionActive property is reachable*/
         state: any;
+
+        curOp: Operation;
+    }
+
+    interface Operation {
+        cm: CodeMirror.Editor;
+
+        /** Flag that indicates that lines might need to be redrawn */
+        viewChanged: boolean;
+
+        /** Used to detect need to update scrollbar */
+        startHeight: number;
+
+        /** Used to force a redraw */
+        forceUpdate: boolean;
+
+        /** Whether to reset the input textarea */
+        updateInput: any;
+
+        /** Whether this reset should be careful to leave existing text (for compositing) */
+        typing: boolean;
+
+        /** Accumulated changes, for firing change events */
+        changeObjs: any;
+
+        /** Set of handlers to fire cursorActivity on */
+        cursorActivityHandlers: any;
+
+        /** Tracks which cursorActivity handlers have been called already */
+        cursorActivityCalled: any;
+
+        /** Whether the selection needs to be redrawn */
+        selectionChanged: boolean;
+
+        /** Set when the widest line needs to be determined anew */
+        updateMaxLine: boolean;
+
+        /** Intermediate scroll position, not pushed to DOM yet */
+        scrollLeft: any, scrollTop: any;
+
+        /** Used to scroll to a specific position */
+        scrollToPos: any;
+
+        /**  */
+        focus: boolean;
+
+        /** Unique ID */
+        id: number;
     }
 
     interface EditorFromTextArea extends Editor {
